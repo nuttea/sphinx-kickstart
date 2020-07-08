55ba1d96b726f8fb26abd8018b76c2c117d8f035
nuttea <nuttea@gmail.com>
2020-07-08 15:32:14 +0700

    auto commit sh

--- commit.sh
+++ commit.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo -e '\n\nClean previous build in 'docs' folder\n'
+echo -e '\n\n*** Clean previous build in 'docs' folder\n'
 rm -Rf docs/*
 
-echo -e '\n\nBuild new stack RTD site and move to 'docs' folder\n'
+echo -e '\n\n*** Build new stack RTD site and move to 'docs' folder\n'
 make html
 mv _build/html/* docs/
 touch docs/.nojekyll
 
-echo -e '\n\nCommit and Push to Master Branch\n'
+echo -e '\n*** Commit and Push to Master Branch\n'
 git add .
 git commit -m "auto commit sh"
 git push origin master
